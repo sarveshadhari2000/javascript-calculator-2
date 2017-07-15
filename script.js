c5b8fa5aaa523de372912b5db580978f8e9417c9
CNDavidson <cndragn@gmail.com>
2017-07-15 02:49:54 -0400

    Get data when caluclator key is pushed

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+$(document).ready(function() {
+
+    //get data when calculator key is pushed
+    document.getElementById('keypad').addEventListener('click', getKey);
+
+    function getKey(k) {
+
+        console.log(k.target.id);
+    }
+
+});
+
+/*
 window.onload = function() {
-	
+    //define numbers
+
+
+
+    //operator functions
+    function add(first, second) {
+        return first + second;
+    }
+
+    function subtract(first, second) {
+        return first - second;
+    }
+
+    function multiply(first, second) {
+        return first * second;
+    }
+
+    function divided(first, second) {
+        return first / second;
+    }
+
+    function percent(first) {
+        return first / 100;
+    }
+
+
+    percent(1, 2);
 }
+*/
